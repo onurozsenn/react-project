d5968002c5d94efe35b47c553924db31468a77f7
onurozsenn <onur@3pounds.io>
2025-05-29 16:14:56 +0300

    Page modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
 import Login from './pages/login-signup/Login';
+import Signup from './pages/login-signup/Signup';
 
 function App() {
   return (
     <div>
       <Login />
+      <Signup />
     </div>
   );
 }
